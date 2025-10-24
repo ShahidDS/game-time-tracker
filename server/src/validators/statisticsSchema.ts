7ba3ece647a71d449bbd98c84e16ba678e0205f7
IsratErin <131592678+IsratErin@users.noreply.github.com>
2025-10-24 13:49:51 +0200

    user statistics zod schema

---
+++ server/src/validators/statisticsSchema.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const userProfileSchema = z.object({
+  id: z.number(),
+  firstName: z.string(),
+  lastName: z.string(),
+  profileImage: z.string().url(),
+});
+
+export const gameStatsItemSchema = z.object({
+  gameName: z.string(),
+  minutesPlayed: z.number(),
+  percentageOfTotal: z.number(),
+});
+
+export const userStatsResponseSchema = z.object({
+  user: userProfileSchema,
+  totalMinutesPlayed: z.number(),
+  gameStats: z.array(gameStatsItemSchema),
+});
