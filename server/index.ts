66db6802d9df689b7f1de84d4f22b2eab7dd9f16
IsratErin <israterin11@gmail.com>
2025-10-25 21:35:51 +0200

    user Statistics

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./src/routes/userRoutes.ts";
-import gameRoutes from "./src/routes/gameRoutes.ts";
+//import gameRoutes from "./src/routes/gameRoutes.ts";
 import statisticsRoutes from "./src/routes/statisticsRoutes.ts";
 
 const app = express();
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
-app.use("/games", gameRoutes);
-app.use("/statistics", statisticsRoutes);
+//app.use("/games", gameRoutes);
+app.use("/userStatistics", statisticsRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
