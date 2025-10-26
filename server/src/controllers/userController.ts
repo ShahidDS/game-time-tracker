17dfc2db127ebe93284f2b42220c25b11a0f554c
kunnikarB <kunnikarboon@gmail.com>
2025-10-26 05:17:50 +0100

    Update Schema : PlaySession add onDelete: Cascade

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -97,6 +97,11 @@ export const deleteUser = async (req: Request, res: Response) => {
     res.status(200).json({ message: 'User deleted successfully'});
   } catch (error) {
     console.error('Error deleting user:', error);
+    
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+      
+      return res.status(404).json({ error: 'User not found' });
+    }
     res.status(500).json({ error: 'Failed to delete user' });
   }
 };
